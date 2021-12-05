d209ab70757eab7e485c0255f7b4555456756bf7
Christopher Conlon <christopherconlon@Chris.local>
2021-12-05 13:13:04 -0500

    COMMENTS

--- fresh_install.sh
+++ fresh_install.sh
@@ -7,6 +7,7 @@ declare -A wanted
 which brew
 [ $? -eq 1 ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Read AppList.conf and question if we want it or not
 function readConf {
 	while read line; do
 		read -q "wants?$line Wanted (y/n): "
@@ -15,6 +16,7 @@ function readConf {
 	done < AppList.conf
 }
 
+# Confirm it looks good
 function listWanted {
 	echo "#---- Verification ----#"
 	for key value in ${(kv)wanted}; do
@@ -25,18 +27,21 @@ function listWanted {
 	[ $cont = "n" ] && echo -e "\nExiting... " && exit 0
 }
 
+# Brew install all
 function installPhase {
 	for key value in ${(kv)wanted}; do
 		[ $value = "y" ] && brew install $key || echo "Skipping $key"
 	done 
 }
 
+# Copy some files to the spots I like
 function copyFilesInPlace {
 	[ -d "$HOME/.config" ] || mkdir "${HOME}/.config"
 	cp -r $pwd/config/* ${HOME}/.config
 	cp -r $pwd/home/* ${HOME}/
 }
 
+# Trigger em!
 readConf
 listWanted
 installPhase
